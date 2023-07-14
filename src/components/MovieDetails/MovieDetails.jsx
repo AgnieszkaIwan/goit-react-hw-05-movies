07830e0a424249a6f50d64cb0fbca4f6cb197bac
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-07-14 18:30:07 +0200

    .

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const MovieDetails = () => {
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}?api_key=c7e16231292b385d81e462e67776cee3`
+        );
+        setMovieDetails(response.data);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [movieId]);
+
+  if (!movieDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{movieDetails.title}</h1>
+      <p>{movieDetails.overview}</p>
+      {/*Render other movie details */}
+    </div>
+  );
+};
+
+export default MovieDetails;
